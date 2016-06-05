81cca741e88c2ad86ad4353ac7eaef5d0ec80ff6
kaziupir <kaziupir@gmail.com>
2016-06-06 01:07:48 +0200

    geolocalization

--- www/js/index.js
+++ www/js/index.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
     $('#cam').click(function(){
         location.href="cam.html";
     });
+
+    $('#geo').click(function(){
+        location.href="geo.html";
+    });
+
+    $('#db').click(function(){
+        location.href="db.html";
+    });
+
+    $('#weather').click(function(){
+        location.href="weather.html";
+    });
 });
